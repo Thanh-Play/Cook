d5ab3de25ccd53c8aaca2beedbd6f5e44fc5bd63
Thanh-Play <69939810+Thanh-Play@users.noreply.github.com>
2023-02-28 19:02:13 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function changePoem() {
+    document.getElementById("poem").innerHTML = "There is no way to improve yourself";
+    document.getElementById("hpoem").innerHTML = "Lie to me";
+}
+
+function changeBack() {
+    document.getElementById("poem").innerHTML = ogPara;
+    document.getElementById("hpoem").innerHTML = ogHead;
+}
